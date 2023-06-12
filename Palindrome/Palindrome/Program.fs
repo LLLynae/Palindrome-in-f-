f3800f9ad01b93e4333ac08d19421e9a99e9cdb0
LLLynae <111822436+LLLynae@users.noreply.github.com>
2023-06-12 13:50:02 -0500

    Add files via upload

---
+++ Palindrome/Palindrome/Program.fs
@@ -0,0 +1,25 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+
+let isPalindrome =
+    printfn "Hi! I will find out if your word is a palindrome or not."
+    printfn "Please enter your word."
+    //Prompts user to enter word
+
+    let userString = System.Console.ReadLine() |> string
+    //Console entry for user's string
+    let stringReverse = System.String(Array.rev (userString.ToCharArray()))
+    //Uses Array.rev to reverse through user's string, reading it backwards
+    //ToCharArray makes string into array of characters so it can be revversed
+
+    if userString = stringReverse then
+        printfn "True! Your word is a palindrome."
+    else printfn "False! Your Word is not a palindrome."
+    //if statement check for if the word is the same forwards and backwards
+   
+    
+    
+        
+
+        
+            
